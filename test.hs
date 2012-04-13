44e9ef79218ba55d1e39df591f616e375de6fb08
Dan Colish <dcolish@gmail.com>
2012-04-12 21:48:12 -0700

    make print more obvious

--- test.hs
+++ test.hs
@@ -2,21 +2,28 @@ import System.Random
 import Data.List hiding (insert)
 import Chapter3 (insert, Color(..), RB(..))
 
+
 show_tree E' = return ()
-show_tree (RB c a y z) = do
+show_tree t@(RB c a y z) = do
   root
-  link a
-  show_tree a 
-  link z
+  link t
+  show_tree a
   show_tree z
   where
     root = case c of
       R -> putStrLn $ (show y) ++ "[style=filled, fillcolor=red];"
       B -> putStrLn $ (show y) ++ "[style=filled, fillcolor=black];"
     link a = case a of
-      RB _ a' y' z' -> do
-        putStrLn $ (show y) ++ " -> " ++ (show y')
+      RB _ (RB _ _ x _) y (RB _ _ z _) -> do
+        putStrLn $ (show y) ++ " -> " ++ (show x)
+        putStrLn $ (show y) ++ " -> " ++ (show z)
+      RB _ (RB _ _ x _) y E' -> do
+        putStrLn $ (show y) ++ " -> " ++ (show x)
+      RB _ E' y (RB _ _ z _) -> do
+        putStrLn $ (show y) ++ " -> " ++ (show z)
+      RB _ E' y E' -> return ()
       E' -> return ()
+
 
 main = do
   putStrLn "digraph tree {"
@@ -32,4 +39,4 @@ main = do
       build_tree [] t = t
       build_tree (x:xs) t = build_tree xs (insert x t)
       randomlist :: Int -> StdGen -> [Int]
-      randomlist n = take n . randomRs (1,1000)
+      randomlist n = take n . randomRs (1, 1000)
